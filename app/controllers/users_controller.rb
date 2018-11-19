143982feb1f222205d20ddb6ea26ad1075f65867
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-11-19 10:59:00 +0800

    Create basic html form and create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
   def new
   end
+
   def create
-    
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if @user.save
+    redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
