9a0e25f8a291d42908dc8dfe55f37c7d39f44b1c
dipto0321 <diptokmk47@gmail.com>
2018-11-19 09:15:40 +0600

    Create a strong params for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
     if @user.save
-    redirect_to @user
+      redirect_to @user
     else
       render 'new'
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
